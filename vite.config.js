ffe9c53657aacb1537431fc8cd5b68b1c7f4da12
Karandeep <bhasinkarandeepsingh826@gmail.com>
2025-07-01 14:43:35 +0530

    Fix base path for GitHub Pages routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// defineConfig can accept a function to get mode
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Terioat-Industries/' : '/',
-  plugins: [react()]
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Terioat-Industries/' : '/',
+  plugins: [react()],
 }));
